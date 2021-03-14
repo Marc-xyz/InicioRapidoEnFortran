f3708266fa3a1fd9696475d9e466ceb46b58fbc3
Marc-xyz <49844756+Marc-xyz@users.noreply.github.com>
2021-03-14 20:35:41 +0100

    Añadido leer_variables.f90

---
+++ Programas/leer_valores.f90
@@ -0,0 +1,9 @@
+        program leer_valores
+                implicit none
+                integer :: edad
+
+                print *, 'Por favor introduzca su edad: '
+                read (*,*) edad
+
+                print *, 'Tu edad es: ', edad
+        end program leer_valores      
